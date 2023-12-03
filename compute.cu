28619989b9b935958275b26cb47464583d78f8c8
NabihaS <nazasy@udel.edu>
2023-12-03 18:03:26 -0500

    using one large 1D array, null output

--- compute.cu
+++ compute.cu
@@ -2,48 +2,124 @@
 #include <math.h>
 #include "vector.h"
 #include "config.h"
+#include <stdio.h>
 
-//compute: Updates the positions and locations of the objects in the system based on gravity.
+
+// make  kernels i.e. functions that have global in front
+// i think a kernel IS intended to be called in parallel
+// then these kernels will be called here
+// i think CUDA will handle the distribution of kernels after the calls, its just about figuring out the dimensions for the blocks and declaring that
+// Q: do i have one grid???
+
+
+	/*Kernel 2 computes arrays*/
+__global__ void computeAccels(vector3* d_accelvalues, vector3* d_Pos, double* d_mass){
+	//first compute the pairwise accelerations.  Effect is on the first argument.
+	// we're just going to compute the index using our massive 1d array (our matrix, with the computed indices)
+	int i = blockIdx.x * blockDim.x + threadIdx.x; 
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+	// so in a grid, the whichBlock*Threadsperblock jumps you to the right block, 
+	// then you add the thread index to get to the right thread in that block. and this is a 2d grid so u do that for the y's too
+	// then need to map it onto the 2d matrix of accels (really a 1d array currently)
+
+	// z dimension?
+	// int z=threadIdx.z;
+	
+	// NOTE. CURRENTLY D_ACCELVALUES IS A 1D ARRAY SO YOU CANT DOUBLE [][] INDEX LIKE ITS A 2D ARRAY. SO ALL CHECKS NEED TO BE ACCORDING
+	// the 1d array is why i changed the i check to num SQUARED, bc thats the full range?
+	if (i < NUMENTITIES*NUMENTITIES && j < NUMENTITIES) { // this means that a specific thread in the allocated grids actually correspond w an entity, bc we may have extra blocks
+		if (i==j) {
+			FILL_VECTOR(d_accelvalues[i*NUMENTITIES+j],0,0,0); // cant use fill vector if u have k
+		}
+		else{
+			vector3 distance;
+			for (int k=0;k<3;k++) distance[k]=d_Pos[i][k]-d_Pos[j][k]; // you need to synthreads here
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*d_mass[j]/magnitude_sq;
+			FILL_VECTOR(d_accelvalues[i*NUMENTITIES+j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);// this uses z index
+		}
+			
+		
+	}
+
+}
+
+/*Kernel 3 does sums, and also updates values if reduction is not happening */
+
+__global__ void sumAccelsAndUpdate(vector3* d_accelvalues, vector3* d_Pos, vector3* d_Vel){ 
+//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+// this is NOT a thread doing the smallest component yet 
+// launch a thread per column and just loop thru each column
+// OR to optimize launch block
+
+// local thing in kernel called sum, calculate sum with single thread, and put that value in the very beginning of accels. OR a temp array 
+// your final sum will still be a vector3, it will be the aggregate x's,y's, z's of all the objects
+// if you do __shared__ then all the threads in the block will be able to access the sum and you can do reduction
+// if you do separate out the sumaccels and update functions then yes u need
+// --to have an array of vector3* d_sums where you keep track of the sums. OR, you could just overwrite them in the first column of accels
+
+// for this one, you only need j to increment right, i is static, and youre walking down the column
+int i = blockIdx.x * blockDim.x + threadIdx.x;
+// we dont need j right? for every "column" we're incrementing j from 0-num
+
+// does this actually need a start and end
+if (i < NUMENTITIES*NUMENTITIES) {
+	vector3 accel_sum={0,0,0};
+	for (int j=0;j<NUMENTITIES;j++){
+		for (int k=0;k<3;k++)
+			accel_sum[k]+=d_accelvalues[i*NUMENTITIES+j][k];
+	}
+
+	// Store the result in the accels array??
+
+	// for now, include update here
+	//compute the new velocity based on the acceleration and time interval
+	//compute the new position based on the velocity and time interval
+	for (int k=0;k<3;k++){
+		d_Vel[i][k]+=accel_sum[k]*INTERVAL;
+		d_Pos[i][k]+=d_Vel[i][k]*INTERVAL;
+	}
+	
+}
+
+}
+
+/*Kernel 4 uses sums to do update
+
+__global__ void update(vector3** accels, vector3* d_Pos, vector3* d_Vel){
+//compute the new velocity based on the acceleration and time interval
+//compute the new position based on the velocity and time interval
+//Q: is there a built in way to do reductions
+	for (i=0;i<NUMENTITIES;i++){
+
+		for (k=0;k<3;k++){
+			d_Vel[i][k]+=accels[k]*INTERVAL;
+			d_Pos[i][k]+=d_Vel[i][k]*INTERVAL;
+		}
+	}
+}
+
+*/
+
+//compute: Kernel invocations on the GPU
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
-	free(accels);
-	free(values);
+
+	// define grid with x blocks
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); // does this need a z? no? bc the threads are the ones that need the z to compute each in the vector?
+	dim3 numBlocks((NUMENTITIES+BLOCK_SIZE-1)/BLOCK_SIZE, (NUMENTITIES+BLOCK_SIZE-1)/BLOCK_SIZE);
+
+	// call kernels
+	// Q: Do i need cudaDeviceSynchronize() anywhere?
+
+	// why do you have to pass in the device variables if theyre allocated on the GPU
+	// need to fundamentally understand the relationship btwn compute.cu and nbody.cu files. what is being accessed
+	computeAccels<<<numBlocks, dimBlock>>>(d_accelvalues, d_Pos, d_mass);
+	sumAccelsAndUpdate<<<numBlocks, dimBlock>>>(d_accelvalues, d_Pos, d_Vel);
+	// update<<numBlocks, dimBlock<<(d_accelvalues,d_Pos, d_Vel);
+
+
 }
--- nbody.cu
+++ nbody.cu
@@ -8,9 +8,11 @@
 #include "compute.h"
 
 // represents the objects in the system.  Global variables
-vector3 *hVel, *d_hVel;
-vector3 *hPos, *d_hPos;
-double *mass;
+vector3 *hVel, *d_Vel; // the d_ values are all for the device (GPU). but while youre doing mallocmanage maybe u only need one. declare this here or in compute?
+vector3 *hPos, *d_Pos;
+double *mass, *d_mass; 
+vector3* d_accelvalues; 
+
 
 //initHostMemory: Create storage for numObjects entities in our system
 //Parameters: numObjects: number of objects to allocate
@@ -19,10 +21,79 @@ double *mass;
 void initHostMemory(int numObjects)
 {
 	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	hPos = (vector3 *)malloc(sizeof(vector3) * numObjects);
+	hPos = (vector3 *)malloc(sizeof(vector3) * numObjects); 
 	mass = (double *)malloc(sizeof(double) * numObjects);
 }
 
+// KEEP THIS HERE
+// you could just have the values array thats size numentities squared, and ur computing values[i*numentities+j] to index each, you dont need to store the pointers
+// YOU NEED TO DECLARE A LOCAL ACCELS ARRAY TO LOOP THRU AND MALLOC FOR EACH temp_accels[i] -- okay lets just have one array
+
+void initDeviceMemory(int numObjects){
+
+	//declare the arrays here, but these will be allocated on the device
+	// Q: declare accelvalues here?
+	
+
+	// if we want to use two arrays, i.e. store pointers in accels
+	/* vector3** d_accels; */
+	/* cudaMalloc(&d_accels,sizeof(vector3)*numObjects); */
+	// you have to cudamalloc for the arrays, and then initialize a temp array and malloc in it and copy it up i think
+	// its just that cudamalloc expects a cpu variable so you cant iterate thru ?
+	// ?? sets up the pointers
+	/* 
+	int i,j;
+	vector3* temp_accels;
+	for (i=0;i<NUMENTITIES;i++)
+		cudaMalloc(&temp_accels[i],sizeof(vector3)?);
+	*/
+	
+	cudaMalloc(&d_accelvalues,sizeof(vector3)*numObjects*numObjects); // cudamalloc returns a pointer to the CPU
+	cudaMalloc(&d_Pos,sizeof(vector3) * numObjects);
+	cudaMalloc(&d_Vel,sizeof(vector3) * numObjects);
+	// cudaMalloc(&d_mass,sizeof(double) * numObjects);
+
+	// never need to memcpy accels
+
+	#ifdef DEBUG
+	cudaError_t cudaError = cudaMalloc(&d_mass, sizeof(double) * numObjects);
+	if (cudaError != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed: %s\n", cudaGetErrorString(cudaError));
+		fflush(stdout); //?
+		// Handle the error or exit the program
+		exit(EXIT_FAILURE);
+	}
+	// else printf("Mem allocated for d_mass!!\n");
+	
+	#endif
+
+}
+
+void loadDeviceMemory(){
+	// assuming the solar system is populated
+	cudaMemcpy (d_Pos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice); 
+	cudaMemcpy (d_Vel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
+
+	#ifdef DEBUG
+	cudaError_t cudaError = cudaMemcpy (d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
+	if (cudaError != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed: %s\n", cudaGetErrorString(cudaError));
+		// Handle the error or exit the program
+		exit(EXIT_FAILURE);
+	}
+	#endif
+	
+}
+
+// OTHER FUNCTIONS: DEFINE A CALLXKERNEL FN FOR EACH KERNEL, WHERE YOU SPECIFY BLOCK SIZE AND NEEDED VARIABLES??
+void freeDeviceMemory(){
+	// if this was using 2 arrays, we would need a loop to iterate thru the accels array and then free
+	// "and even when you free it you have to bring it down to CPU in another temp accels and loop thru and free each part and then free the accels"
+	cudaFree(d_accelvalues);
+	cudaFree(d_Pos);
+	cudaFree(d_Vel);
+	cudaFree(d_mass);
+}
 //freeHostMemory: Free storage allocated by a previous call to initHostMemory
 //Parameters: None
 //Returns: None
@@ -91,25 +162,34 @@ void printSystem(FILE* handle){
 
 int main(int argc, char **argv)
 {
+	printf("NUMENTITIES IS %d\n", NUMENTITIES);
 	clock_t t0=clock();
 	int t_now;
 	//srand(time(NULL));
 	srand(1234);
 	initHostMemory(NUMENTITIES);
+	initDeviceMemory(NUMENTITIES);
 	planetFill();
 	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
 	//now we have a system.
 	#ifdef DEBUG
 	printSystem(stdout);
+	printf("This is our solar system.\n");
 	#endif
-	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
-		compute();
+	// Now we want to load what we need from our system into the device
+	loadDeviceMemory();
+	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){	
+		compute(); // we want all the memory transfers to happen outside of this loop
 	}
+	// copy relevant data to host. CPU is making this call and the CUDA API is handling the GPU actions
+	cudaMemcpy ( hPos, d_Pos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
+	cudaMemcpy ( hVel, d_Vel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost);
 	clock_t t1=clock()-t0;
 #ifdef DEBUG
 	printSystem(stdout);
 #endif
 	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
-
+	// free device memory here
+	freeDeviceMemory();
 	freeHostMemory();
 }
